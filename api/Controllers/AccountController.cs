834d267ccf4a458dc1e3cf127b90b208525ef8dc
mdtthanh <maithanh0131@gmail.com>
2025-01-13 12:46:31 +0700

    register account user

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using api.Models;
-using api.Dtos.Account; // Assuming AppUser is defined in the Models namespace
+using api.Dtos.Account;
+using api.Interfaces; // Assuming AppUser is defined in the Models namespace
 
 namespace api.Controllers
 {
@@ -14,11 +15,12 @@ namespace api.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
-        public AccountController(UserManager<AppUser> userManager)
+        private readonly ITokenService _tokenService;
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
         {
             _userManager = userManager;
+            _tokenService = tokenService;
         }
-
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
         {
@@ -26,12 +28,30 @@ namespace api.Controllers
             {
                 if (!ModelState.IsValid)
                     return BadRequest(ModelState);
+
+                if (string.IsNullOrEmpty(registerDto.Email))
+                {
+                    return BadRequest("Email cannot be null or empty.");
+                }
+
+                var existingUser = await _userManager.FindByEmailAsync(registerDto.Email);
+
+                if (existingUser != null)
+                {
+                    return BadRequest("Email is already registered");
+                }
+
                 var appUser = new AppUser
                 {
                     UserName = registerDto.UserName,
                     Email = registerDto.Email,
 
                 };
+
+                if (string.IsNullOrEmpty(registerDto.Password))
+                {
+                    return BadRequest("Password cannot be null or empty.");
+                }
 
                 var createdUser = await _userManager.CreateAsync(appUser, registerDto.Password);
 
@@ -40,7 +60,14 @@ namespace api.Controllers
                     var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
                     if (roleResult.Succeeded)
                     {
-                        return Ok("User created");
+                        return Ok(
+                            new NewUserDto
+                            {
+                                UserName = appUser.UserName ?? string.Empty,
+                                Email = appUser.Email ?? string.Empty,
+                                Token = _tokenService.CreateToken(appUser)
+                            }
+                        );
                     }
                     else
                     {
